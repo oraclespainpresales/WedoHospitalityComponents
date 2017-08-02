9741cb5afc981133d5c0bd54a73bc3fc1d1ab570
Oracle Spain Presales <oraclespainpresales@gmail.com>
2017-08-02 16:20:30 +0200

    modify get room number process

--- sh/getRoomNumber.js
+++ sh/getRoomNumber.js
@@ -30,11 +30,11 @@ module.exports = {
 		}
 		
 			
-		var Client = require('node-rest-client').Client;
+	/*	var Client = require('node-rest-client').Client;
 		var client = new Client();
 		var social1 = sdk.variable('profile.firstName');
-		/*var social2 = sdk.variable('profile.lastName');		
-		var social = social1+social2;		*/
+		//var social2 = sdk.variable('profile.lastName');		
+		//var social = social1+social2;		
 		var corrId = sdk.variable('corrId');			
 		console.log("corrId: "+corrId);		
 		
@@ -76,7 +76,56 @@ module.exports = {
 		
 		req.on('error', function (err) {
 			console.log('request error', err);
-		});			
+		});*/
+
+
+
+
+		var Client = require('node-rest-client').Client;
+		var client = new Client();
+		var social1 = sdk.variable('profile.firstName');
+		var social2 = sdk.variable('profile.lastName');		
+		var social = social1+social2;		
+		//social= "CarlosCasares";		
+		
+		var args = {			
+			headers: { "Content-Type": "application/json", "Accept": "application/json"}
+		};
+		
+		//console.log("args:"+JSON.stringify(args));
+		
+		var req=client.get("http://new.proxy.digitalpracticespain.com:9997/ords/pdb1/smarthospitality/stay/search/"+social, args,function (data, response) {
+		
+			//	console.log("DATA:::" +JSON.stringify(data));					
+				//console.log("DATA:::" +response.statusCode);	
+				if ((response.statusCode==200))
+				{
+					sdk.reply({text: social1+ ", it's your room number: "+data.roomid});
+					/*if ((data.targetTemperature!=null) && (data.currentTemperature!=null))
+						sdk.reply({text: "Your have done a special request about room temperature. Current: "+data.targetTemperature+"ºC Target: "+data.currentTemperature+"ºC"});			*/
+					sdk.action('success');        
+					sdk.done(true);	
+					done(sdk);					
+				}else{
+					sdk.reply({text: "Currently we can provide you a room number. Please, try again later."});			
+					sdk.action('success');        
+					sdk.done(true);	
+					done(sdk);	
+				}
+		});
+		
+		req.on('requestTimeout', function (req) {
+			console.log('request has expired');
+			req.abort();
+		});
+		 
+		req.on('responseTimeout', function (res) {
+			console.log('response has expired');		 
+		});		
+		
+		req.on('error', function (err) {
+			console.log('request error', err);
+		});		
 				
 	}
 			
