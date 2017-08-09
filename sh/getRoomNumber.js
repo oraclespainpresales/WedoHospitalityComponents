daa0b379454f342a631d7c9d89ac48c49ca83559
Oracle Spain Presales <oraclespainpresales@gmail.com>
2017-08-09 10:34:33 +0200

    temperature
    added current and target temperature properties

--- sh/getRoomNumber.js
+++ sh/getRoomNumber.js
@@ -45,20 +45,31 @@ module.exports = {
 			headers: { "Content-Type": "application/json", "Accept": "application/json"}
 		};
 		
-		//console.log("args:"+JSON.stringify(args));
-		//console.log("http://new.proxy.digitalpracticespain.com:9997/ords/pdb1/smarthospitality/stay/search/"+encodeURIComponent(social));
-		var req=client.get("http://new.proxy.digitalpracticespain.com:9997/ords/pdb1/smarthospitality/stay/search/"+encodeURIComponent(social), args,function (data, response) {
+		
+		
+		var req=client.get("http://new.soa.digitalpracticespain.com/room/"+encodeURIComponent(social), args,function (data, response) {
 		
 			//	console.log("DATA:::" +JSON.stringify(data));					
 				console.log("DATA:::" +response.statusCode);	
 				if ((response.statusCode==200))
 				{
-					sdk.reply({text: social1+ ", This is your room number: "+data.roomid});
-					/*if ((data.targetTemperature!=null) && (data.currentTemperature!=null))
-						sdk.reply({text: "Your have done a special request about room temperature. Current: "+data.targetTemperature+"ºC Target: "+data.currentTemperature+"ºC"});			*/
-					sdk.action('success');        
-					sdk.done(true);	
-					done(sdk);					
+					if (data.hasOwnProperty('roomid'))
+					{
+						sdk.reply({text: social1+ ", This is your room number: "+data.roomid});
+						if ((data.temperature!=null))
+							{
+								sdk.reply({text: "Your have done a special request about room temperature. Current: "+data.temperature.current+"ºC Target: "+data.temperature.target+"ºC"});			
+							}
+						sdk.action('success');        
+						sdk.done(true);	
+						done(sdk);										
+					
+					}else{
+						sdk.reply({text: "Currently we can't provide you a room number. Please, try again later."});			
+						sdk.action('success');        
+						sdk.done(true);	
+						done(sdk);	
+					}					
 				}else{
 					sdk.reply({text: "Currently we can't provide you a room number. Please, try again later."});			
 					sdk.action('success');        
