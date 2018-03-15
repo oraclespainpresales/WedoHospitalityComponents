b6e125f84c687a261ac3844f736af4410edfb2df
Oracle Spain Presales <oraclespainpresales@gmail.com>
2018-03-15 12:28:29 +0100

    Add files via upload

--- registry.js
+++ registry.js
@@ -4,37 +4,37 @@ module.exports = {
   components: {
 
 ////Smart Hospitality - Show Detail and Booking Flows
-	  'hospitality.showHotelDetail' : require('./hospitality/showHotelDetail'),
-	  'hospitality.search' : require('./hospitality/search'),
-	  'hospitality.registerBooking' : require('./hospitality/registerBooking'),
-	  'hospitality.newcustomer' : require('./hospitality/newcustomer'),
-	  'hospitality.payment' : require('./hospitality/payment'),
-	  'hospitality.showPaymentMethod' : require('./hospitality/showPaymentMethod'),			  
+	  'hospitality.showHotelDetail' : require('./sh/showHotelDetail'),
+	  'hospitality.search' : require('./sh/search'),
+	  'hospitality.registerBooking' : require('./sh/registerBooking'),
+	  'hospitality.newcustomer' : require('./sh/newcustomer'),
+	  'hospitality.payment' : require('./sh/payment'),
+	  'hospitality.showPaymentMethod' : require('./sh/showPaymentMethod'),			  
 	  //Smart Hospitality - Pre-checking Flow
-	  'hospitality.precheckinflow' : require('./hospitality/precheckinflow'),
-	  'hospitality.requestPhoto' : require('./hospitality/requestPhoto'),
-	  'hospitality.saveComments' : require('./hospitality/saveComments'),
-	  'hospitality.updateSocialID' : require('./hospitality/updateSocialID'),			  
+	  'hospitality.precheckinflow' : require('./sh/precheckinflow'),
+	  'hospitality.requestPhoto' : require('./sh/requestPhoto'),
+	  'hospitality.saveComments' : require('./sh/saveComments'),
+	  'hospitality.updateSocialID' : require('./sh/updateSocialID'),			  
 	  //Smart Hospitality - Room Number Flow
-	  'hospitality.' : require('./hospitality/getRoomNumber'),			  
+	  'hospitality.' : require('./sh/getRoomNumber'),			  
 	  //Smart Hospitality - Set Room Temp. Flow
-	  'hospitality.setRoomTemp' : require('./hospitality/setRoomTemp'),			  
+	  'hospitality.setRoomTemp' : require('./sh/setRoomTemp'),			  
 	  //Smart Hospitality - Open/Close Door Flow
-	  'hospitality.openDoor' : require('./hospitality/openDoor'),  
-	  'hospitality.closeDoor' : require('./hospitality/closeDoor'),			  
+	  'hospitality.openDoor' : require('./sh/openDoor'),  
+	  'hospitality.closeDoor' : require('./sh/closeDoor'),			  
 	  //Smart Hospitality - Get Info Service Flow  
-	  'hospitality.getInfoService' : require('./hospitality/getInfoService'),
-	  'hospitality.getAllAvailableServices' : require('./hospitality/getAllAvailableServices'),  
-	  'hospitality.MakeOrder' : require('./hospitality/MakeOrder'),			 
+	  'hospitality.getInfoService' : require('./sh/getInfoService'),
+	  'hospitality.getAllAvailableServices' : require('./sh/getAllAvailableServices'),  
+	  'hospitality.MakeOrder' : require('./sh/MakeOrder'),			 
 	  //Smart Hospitality - Checkout Flow
-	  'hospitality.checkout' : require('./hospitality/checkout'),
-	  'hospitality.saveFeedback' : require('./hospitality/saveFeedback'),
-	  'hospitality.invoiceConfirmed' : require('./hospitality/invoiceConfirmed'),
-	  'hospitality.feedbackQuestion' : require('./hospitality/feedbackQuestion'),			  
+	  'hospitality.checkout' : require('./sh/checkout'),
+	  'hospitality.saveFeedback' : require('./sh/saveFeedback'),
+	  'hospitality.invoiceConfirmed' : require('./sh/invoiceConfirmed'),
+	  'hospitality.feedbackQuestion' : require('./sh/feedbackQuestion'),			  
 	   //Smart Hospitality - ASK WeDo Points
-	  'hospitality.getWedoPoints' : require('./hospitality/getWedoPoints'),			  
+	  'hospitality.getWedoPoints' : require('./sh/getWedoPoints'),			  
 	  //Helper
-	  'hospitality.assignVariable' : require('./hospitality/assignVariable'),
-	  'hospitality.conditionFromLastMessage' : require('./hospitality/conditionFromLastMessage')
+	  'hospitality.assignVariable' : require('./sh/assignVariable'),
+	  'hospitality.conditionFromLastMessage' : require('./sh/conditionFromLastMessage')
   }
 };
